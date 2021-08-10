9cb4e38f4d0218510c2f966cac46136c6ecbc4a4
nemborkar <swarnimborkar@gmail.com>
2021-08-10 18:29:59 +0900

    Tried using regular expressions to filter html

--- base.py
+++ base.py
@@ -1,9 +1,16 @@
 from urllib.request import urlopen
+import re
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
+url = "http://olympus.realpython.org/profiles/poseidon"
 page = urlopen(url)
 
 html_byte = page.read()
 html = html_byte.decode("utf-8")
 
-print(html)
+
+pattern = "<title.*?>.*?</title.*?>"
+match_results = re.search(pattern, html, re.IGNORECASE)
+title = match_results.group()
+title = re.sub("<.*?>", "", title)
+
+print(title)
